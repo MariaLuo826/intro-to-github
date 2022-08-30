609c0600ed9c5852df60ea3b016366e71edb5780
MariaLuo826 <112397482+MariaLuo826@users.noreply.github.com>
2022-08-30 09:22:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def example():
   print("It's way too hot in here")
 def cats ():
-  name = input ("what is your cat's name")
-  print (name)
+  name = input ("what is your cat's name ")
+  print (name + " is a great name.")
